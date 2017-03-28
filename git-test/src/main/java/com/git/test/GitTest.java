2ff5f5cd767afda821c0bffc5ca1c412810a3a00
Leo <493985914@qq.com>
2017-03-28 14:10:35 +0800

    第一个文件提交到git上

---
+++ git-test/src/main/java/com/git/test/GitTest.java
@@ -0,0 +1,11 @@
+package com.git.test;
+
+/**
+ * Created by liaoqianyang on 2017/3/28.
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello git");
+    }
+}
