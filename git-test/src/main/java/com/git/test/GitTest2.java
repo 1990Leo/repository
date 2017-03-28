a37143334553861ac4d99bd3a107ebe3a1d1cc31
Leo <493985914@qq.com>
2017-03-28 14:19:01 +0800

    第二个文件提交到git上

---
+++ git-test/src/main/java/com/git/test/GitTest2.java
@@ -0,0 +1,10 @@
+package com.git.test;
+
+/**
+ * Created by liaoqianyang on 2017/3/28.
+ */
+public class GitTest2 {
+    public static void main(String[] args) {
+        System.out.println("Hello git2");
+    }
+}
