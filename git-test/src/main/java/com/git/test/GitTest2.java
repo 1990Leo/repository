43da4c9238fc5d15a49e0eb83a4a5b08b08fede6
Leo <493985914@qq.com>
2017-03-28 14:41:58 +0800

    第三个文件提交到git上

--- git-test/src/main/java/com/git/test/GitTest2.java
+++ git-test/src/main/java/com/git/test/GitTest2.java
@@ -7,5 +7,6 @@ public class GitTest2 {
     public static void main(String[] args) {
         System.out.println("Hello git2");
         System.out.println(1);
+        System.out.println("modify feature");
     }
 }
